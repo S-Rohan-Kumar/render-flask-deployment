feff7be3c065cc8d4e5da95459f1bbb5c080572f
S-Rohan-Kumar <rohankumar04102005@gmail.com>
2025-04-12 20:20:28 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # Install system dependencies
 apt-get update
-apt-get install -y tesseract-ocr ffmpeg libmysqlclient-dev
+apt-get install -y tesseract-ocr tesseract-ocr-eng ffmpeg libmysqlclient-dev
+
+# Verify Tesseract installation
+tesseract --version
+which tesseract
 
 # Exit on error
 set -o errexit
